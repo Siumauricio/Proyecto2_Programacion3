4beb80e3a1e6d305f7499aaef5ad8a5a321aa7dc
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2019-09-15 22:47:07 -0600

    Update Archivo.h

--- Archivo.h
+++ Archivo.h
@@ -1,18 +1,21 @@
 #pragma once
 #pragma once
 #include "ListaEnlazada.h"
+#ifndef ARCHIVO_H
+#define ARCHIVO _H
 class archivo {
-
 public:
 	int fila;
 	int columna;
 	ListaEnlazada* matriz_1(string);
-	ListaEnlazada*  matriz_2(string);
+	ListaEnlazada* matriz_2(string);
 	void leerArchivo();
 	void asFila(int);
 	int obFila();
 	void asColumna(int);
 	int obColumna();
 	void imprimir(string nombre);
-	void escribirArchivo(int fila, int columna, ListaEnlazada* lista,int );
+	void escribirArchivo(int fila, int columna, ListaEnlazada* lista,string );
+	void escribirDet(int fila, int columna, ListaEnlazada* lista, string);
 };
+#endif // !ARCHIVO_H
